3c510bd1f7804632b55da56fd53dfdf189358afb
JuukoFrancis <juukojuniorfrancis@gmail.com>
2026-02-27 12:45:48 +0300

    Her section fix

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -12,7 +12,7 @@ const HeroSection = () => {
           <h1 className="text-4xl md:text-6xl font-bold tracking-tight">
             <span className="opacity-0 animate-fade-in"> Hi, I'm</span>
             <span className="text-primary opacity-0 animate-fade-in-delay-1">
-              {"           "}Juuko
+              {"           "}Juko
             </span>
             <span className="text-gradient ml-2 opacity-0 animate-fade-in-delay-2">
               Francis
